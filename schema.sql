db6b68e0e7c7cd42f69e8bf99e718631643f382c
osamaalkarmi20 <osama.alkarmi20@outlook.com>
2023-05-16 17:07:32 +0300

    a

--- schema.sql
+++ schema.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS  addedmv (
     title VARCHAR(255),
     release_date VARCHAR(255), 
    poster_path VARCHAR(255), 
-   overview VARCHAR(255)
+   overview VARCHAR(255), 
+   comment VARCHAR(255)
    
-)
+);
